0ebfcdd21f0002f229f66160d661979b202235c4
张亚飞 <672233580@qq.com>
2022-04-15 14:16:56 +0800

    Initial Commit

---
+++ AdvancedAnimation0415/AdvancedAnimation0415App.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedAnimation0415App.swift
+//  AdvancedAnimation0415
+//
+//  Created by 张亚飞 on 2022/4/15.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedAnimation0415App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
